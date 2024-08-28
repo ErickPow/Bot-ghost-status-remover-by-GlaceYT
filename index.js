145d9246665091d4bfcea3843aa8c30dd744de3b
ErickPow <equspowergame2@gmail.com>
2024-08-28 14:29:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Code: msrpil","Playing Moon State", "View Moon State"];
 
 
 let currentIndex = 0;
