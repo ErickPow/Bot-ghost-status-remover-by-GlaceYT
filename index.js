3bb257598a3c8ee511db4ff4995b9a457a632dca
ErickPow <equspowergame2@gmail.com>
2024-08-30 11:16:47 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Playing EvilMC","Jugando EvilMC"];
 
 
 let currentIndex = 0;
